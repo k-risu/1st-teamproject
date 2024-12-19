52587179f0772eb5a2e3f9e4199a44b981d3b2ac
lyss91 <leeyss1991@gmail.com>
2024-12-19 17:33:32 +0900

    [][Feat] SignIn 로그인 기능 구현 (signin폴더 생성 후 작업)

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout";
+import SignIn from "./pages/signin/SignIn";
 
 function App() {
   return (
     <>
       <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" />
-          </Routes>
-        </Layout>
+        {/* <Layout> */}
+        <Routes>
+          {/* 루트 경로에 SignIn 컴포넌트 추가 */}
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+        {/* </Layout> */}
       </Router>
     </>
   );
