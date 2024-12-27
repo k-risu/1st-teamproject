f375f21395de7a28de7e7012d2346def447fd676
lyss91 <leeyss1991@gmail.com>
2024-12-27 14:36:37 +0900

    [feat] vite.config.js 설정 & signin 페이지 및 popup 수정

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,6 @@ const createProxy = (baseURL) => {
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: createProxy("http://192.168.0.144:5213"), // 기본 URL 설정
+    proxy: createProxy("http://192.168.0.144:5213/api/"), // 기본 URL 설정
   },
 });
