1eddb0f60a383dbfa844f43e2210e75b41508671
k-risu <kkd4869@gmail.com>
2024-12-27 14:36:24 +0900

    [feat] 프로젝트 구성원 페이지 모달 추가 및 로그인페이지 수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// 동적 프록시 설정 함수
+const createProxy = (baseURL) => {
+  return {
+    "/api": {
+      target: baseURL, // 기본 백엔드 URL
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, ""), // '/api'를 제거하여 백엔드로 전달
+    },
+  };
+};
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: createProxy("http://192.168.0.144:5213/api/"), // 기본 URL 설정
+  },
 });
