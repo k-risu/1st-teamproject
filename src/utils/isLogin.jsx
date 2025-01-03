b71cd370f6aeda4454f374db0b597151f1425047
213am <dev.213am@gmail.com>
2025-01-03 16:25:21 +0900

    [feat] isLogin 기능 추가

---
+++ src/utils/isLogin.jsx
@@ -0,0 +1,37 @@
+import Swal from "sweetalert2";
+
+export const isLogin = async ({ navigate, cookies }) => {
+  const isCookie = await cookies.signedUserNo;
+
+  if (isCookie === "undefined") {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: "center",
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      },
+    });
+    Toast.fire({
+      icon: "error",
+      title: "로그인이 필요합니다",
+    });
+    setTimeout(() => {
+      navigate(-1);
+    }, 2000);
+  } else {
+    return;
+  }
+};
+
+// 쿠키의 signedUserNo에 값이 없으면 로그인 하라는 알림창과 함께 이전 페이지로 이동
+
+// const [cookies] = useCookies("signedUserNo");
+// const navigate = useNavigate();
+
+// useEffect(() => {
+//   isLogin({ navigate, cookies });
+// }, []);
